d8e8e871472dc66aec4c82e7d7013c06bde14747
Allison Dietz <dietz.allison@outlook.com>
2021-01-19 01:24:14 -0700

    Set up struture of Fetch-api calls

---
+++ src/Fetch-API.js
@@ -0,0 +1,58 @@
+let fetchApi = {
+
+  fetchCurrentTravelerData(userID) {
+    let fetchedCurrentTravelerData = fetch('http://localhost:3001/api/v1/travelers/${userID}')
+      .then(response => response.json())
+      .then(data => data.travelers.find(traveler => traveler.id === userID))
+      .catch(error => console.log(error.message));
+    return fetchedCurrentTravelerData;
+  },
+
+  fetchAllTravelersData() {
+    let fetchedAllTravelersData = fetch('http://localhost:3001/api/v1/travelers')
+      .then(response => response.json())
+      .then(data => data.travelers)
+      .catch(error => console.log(error.message));
+    return fetchedAllTravelersData;
+  },
+
+  fetchTripsData() {
+    let fetchedTripsData = fetch('http://localhost:3001/api/v1/trips')
+      .then(response => response.json())
+      .then(data => data.trips)
+      .catch(error => console.log(error.message));
+    return fetchedTripsData;
+  },
+
+  fetchDestinationsData() {
+    let fetchedDestinationsData = fetch('http://localhost:3001/api/v1/destinations')
+      .then(response => response.json())
+      .then(data => data.destinations)
+      .catch(error => console.log(error.message));
+    return fetchedDestinationsData;
+  },
+
+  postNewTrip(tripRequest, tripDate) {
+    // let newBooking = fetchApi.createBooking(booking);
+    let postTripData = fetch('http://localhost:3001/api/v1/trips', {
+      method: 'POST',
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        "userID": parseInt(tripRequest.userID),
+        "destinationID": parseInt(tripRequest.destinationID),
+        "travelers": parseInt(tripRequest.userID),
+        "date": `${tripDate}`,
+        "duration": parseInt(tripRequest.duration),
+        "status": "pending",
+        "suggestedActivities": []
+      })
+    })
+      .then(response => response.json())
+      .catch(error => console.log(error.message))
+  }
+
+}
+
+export {fetchApi};
